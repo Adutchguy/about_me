1344a42e08175bcb691df55a53d50a658205e420
Adutchguy <mdm_8@hotmail.com>
2017-03-09 15:17:26 -0800

    Got my day 3 work done...finally

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
+// CONFIGURATION
+
 'use strict';
 
-// Question 1
+// GLOBAL VARIABLES
+
+var userNamePrompt = 'Hello, would you mind providing your name?';
+var userName = prompt(userNamePrompt);
+var modUserName = userName[0].toUpperCase() + userName.slice(1, userName.length).toLowerCase();
+var namePromptReply = 'Thank you, ' + modUserName + ' for providing your name';
+alert(namePromptReply);
+var userScore = 0;;
+
+// // //FUNCTIONS// // //
+
+// QUESTION1
 
 var question1 = 'Does Michael think that trump is a good president?';
 var answer1 = prompt(question1);
-if ( answer1 === null) {
-  var userCancel = 'You can play the guessing game another time. :)';
+
+if (answer1 === null) {
+  var userCancel = 'You can play the guessing game another time. Enjoy the site, ' + modUserName + ' .:)';
   console.log(userCancel);
   alert(userCancel);
 }
@@ -14,6 +28,7 @@ if (answer1.toLowerCase() === 'yes' || answer1.toLowerCase() === 'y') {
   var trueResponse = 'hm...I don\'t know about that... But you are entitled to your opinion my friend.';
   console.log(trueResponse);
   alert(trueResponse);
+  userScore++;
 } else if (answer1.toLowerCase() === 'no' || answer1.toLowerCase() === 'n') {
   var falseResponse = 'Alright, I\'m inclined to agree with you.';
   console.log(falseResponse);
@@ -24,15 +39,22 @@ if (answer1.toLowerCase() === 'yes' || answer1.toLowerCase() === 'y') {
   alert(otherResponse);
 }
 
-// Question 2
+// QUESTION2
 
 var question2 = 'Does Michael like dogs?';
 var answer2 = prompt(question2);
+
+if (answer2 === null) {
+  var userCancel = 'You can finish playing the guessing game another time. Enjoy the site, ' + modUserName + ' .:)';
+  console.log(userCancel);
+  alert(userCancel);
+}
 
 if (answer2.toLowerCase() === 'yes' || answer2.toLowerCase() === 'y') {
   trueResponse = 'Wrong! Go suck a lemon.';
   console.log(trueResponse);
   alert(trueResponse);
+  userScore++;
 } else if (answer2.toLowerCase() === 'no' || answer2.toLowerCase() === 'n') {
   falseResponse = 'That\'s right! Cats are easily the best and it can be no other way.';
   console.log(falseResponse);
@@ -43,15 +65,21 @@ if (answer2.toLowerCase() === 'yes' || answer2.toLowerCase() === 'y') {
   alert(otherResponse);
 };
 
-// Question 3
+// QUESTION3
 
 var question3 = 'Does Michael like to play soccer?';
 var answer3 = prompt(question3);
+if (answer3 === null) {
+  var userCancel = 'You can finish playing the guessing game another time. Enjoy the site, ' + modUserName + ' .:)';
+  console.log(userCancel);
+  alert(userCancel);
+}
 
 if (answer3.toLowerCase() === 'yes' || answer3.toLowerCase() === 'y') {
   trueResponse = 'Good guess! I see you were paying attention during our introductions.';
   console.log(trueResponse);
   alert(trueResponse);
+  userScore++;
 } else if (answer3.toLowerCase() === 'no' || answer3.toLowerCase() === 'n') {
   falseResponse = 'We should get to know eachother a little better it seems...';
   console.log(falseResponse);
@@ -62,15 +90,21 @@ if (answer3.toLowerCase() === 'yes' || answer3.toLowerCase() === 'y') {
   alert(otherResponse);
 };
 
-// Question 4
+// QUESTION4
 
 var question4 = 'Is Michael married?';
 var answer4 = prompt(question4);
+if (answer4 === null) {
+  var userCancel = 'You can finish playing the guessing game another time. Enjoy the site, ' + modUserName + ' .:)';
+  console.log(userCancel);
+  alert(userCancel);
+}
 
 if (answer4.toLowerCase() === 'yes' || answer4.toLowerCase() === 'y') {
   trueResponse = 'YEAH YEAH YEAH, I am married to a beautiful Filipina woman.';
   console.log(trueResponse);
   alert(trueResponse);
+  userScore++;
 } else if (answer4.toLowerCase() === 'no' || answer4.toLowerCase() === 'n') {
   falseResponse = 'Sorry, that\'s not right. Though, while I love being married, there ARE days I wish I was single... ';
   console.log(falseResponse);
@@ -81,15 +115,21 @@ if (answer4.toLowerCase() === 'yes' || answer4.toLowerCase() === 'y') {
   alert(otherResponse);
 };
 
-// Question 5
+// QUESTION5
 
 var question5 = 'Does Michael have a child?';
 var answer5 = prompt(question5);
+if (answer5 === null) {
+  var userCancel = 'You can finish playing the guessing game another time. Enjoy the site, ' + modUserName + ' .:)';
+  console.log(userCancel);
+  alert(userCancel);
+}
 
 if (answer5.toLowerCase() === 'yes' || answer5.toLowerCase() === 'y') {
   trueResponse = 'That\'s right! I have an adorable little girl.';
   console.log(trueResponse);
   alert(trueResponse);
+  userScore++;
 } else if (answer5.toLowerCase() === 'no' || answer5.toLowerCase() === 'n') {
   falseResponse = 'We REALLY need to get to know eachother better. I have a little girl. She\'s a real sweetheart';
   console.log(falseResponse);
@@ -100,67 +140,88 @@ if (answer5.toLowerCase() === 'yes' || answer5.toLowerCase() === 'y') {
   alert(otherResponse);
 };
 
-// Question 6
+// QUESTION6
 
 var today = new Date();
 var year = today.getFullYear() - 1;
 var correctAnswer = (year - 1986);
+var attempts = 0;
 
-for (var i = 3; i >= 0; i--) {
-  var question6 = 'How old is Michael? You have ' + [i + 1] + ' attempts.';
+for (var attempts = 3; attempts >= 0; attempts--) {
+  var question6 = 'How old is Michael? You have ' + [attempts + 1] + ' attempts.';
   var answer6 = prompt(question6);
+
   if (parseInt(answer6) === correctAnswer) {
     var trueResponse = 'HOLY %$#^, great guess!';
     console.log(trueResponse);
     alert(trueResponse);
+    userScore++;
     break;
   }
   else if (answer6 > correctAnswer) {
-    var tooHigh = 'Too high. You have ' + [i] + ' attempts remaining.';
+    var tooHigh = 'Too high. You have ' + [attempts] + ' attempts remaining.';
     console.log(tooHigh);
     alert(tooHigh);
   } else if (answer6 < correctAnswer) {
-    var tooLow = 'Too low. You have ' + [i] + ' attempts remaining.';
+    var tooLow = 'Too low. You have ' + [attempts] + ' attempts remaining.';
     console.log(tooLow);
     alert(tooLow);
   }
 }
 
-if (i === -1) {
+if (attempts === -1) {
   var fail = 'You\'re all out of attempts. Better luck next time!';
   console.log(fail);
   alert(fail);
 }
 
-// Question 7
-// add a seventh question to my guessing game that has multiple possible correct answers that are stored in an array. For instance, "Can you guess a state that I have lived in besides Washington?", so that even more of my programming skills are showcased. For now, I will structure this question so that the user has six tries to get a single correct answer, and upon using up those tries OR getting a correct answer, displays a message to the user indicating all of the possible correct answers.
+// QUESTION7
 
-var correctAnswer = ['Tahiti','Hawaii','Las Vegas'];
+var correctAnswer = ['Tahiti','Hawaii','Japan'];
+var question7 = 'What is one of Michael\'s top 3 travel destinations? You have 6 attempts to guess just one of the destinations. Good luck!';
 
-for (var i = 5; i >= 0; i--) {
-  var question7 = 'What is one of Michael\'s top 3 travel destinations? You have ' + [i + 1] + ' attempts to guess just one of the destinations. Good luck!';
-  var answer7 = prompt(question7).toLowerCase();
-  if (answer7 === correctAnswer[0].toLowerCase() || answer7 === correctAnswer[1].toLowerCase() || answer7 === correctAnswer[2].toLowerCase()) {
+for (var attempts = 5; attempts >= 0; attempts--) {
+  var answer7 = prompt(question7);
+  var modAnswer7 = answer7[0].toUpperCase() + answer7.slice(1, answer7.length).toLowerCase();
+
+  if (correctAnswer.includes(modAnswer7)) {
     var trueResponse = 'I\'m impressed, you got one!';
     console.log(trueResponse);
     alert(trueResponse);
+    userScore++;
     break;
   } else {
-    var badAnswer = 'Just plain wrong. You have ' + [i] + ' attempts remaining.';
+    var badAnswer = 'Just plain wrong. You have ' + [attempts] + ' attempts remaining.';
     console.log(badAnswer);
     alert(badAnswer);
   }
 
-  if (i === 0) {
+  if (attempts === 0) {
     var fail = 'You\'re all out of attempts. Better luck next time!';
     console.log(fail);
     alert(fail);
   }
 }
 
-alert('Here are the answers...');
+alert('Here are the answers in order of most desirable to least.');
 
 for (var j = 0; j < correctAnswer.length; j++){
   console.log(correctAnswer[j]);
   alert('Answer ' + (j + 1) + ': ' + correctAnswer[j]);
+}
+
+// USERREPORT
+
+if (userScore === 7) {
+  var scorePerfect = 'You may know me too well, ' + modUserName + '...You got 7 out of 7!';
+  alert(scorePerfect);
+  console.console.log(scorePerfect);
+} else if (userScore < 7 && userScore > 4) {
+  var scoreMedium = 'You know me about as well as the average person, ' + modUserName + '. You scored ' + userScore + ' out of 7. Good job!';
+  alert(scoreMedium);
+  console.log(scoreMedium);
+} else if (userScore < 5) {
+  var scoreLow = 'Hm...This is awkward, ' + modUserName + '. You scored ' + userScore + ' out of 7. Perhaps we need to hang out.';
+  alert(scoreLow);
+  console.log(scoreLow);
 }
